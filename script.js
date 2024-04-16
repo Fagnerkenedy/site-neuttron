7522ed69bf6aa19319b703e68c9d1df6f9c4f97c
Fagner Kenedy <fagnerkenedy@hotmail.com>
2024-04-16 20:35:40 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
+function scrollToID(event) {
+    event.preventDefault();
+    const ID = event.currentTarget.getAttribute('href');
+    const target = document.querySelector(ID);
+    target.scrollIntoView({ behavior: "smooth" });
+    // fecha o menu
+    btnMobile.click();
+  }
+  
+  // Seleciona links internos
+  const intLinks = document.querySelectorAll('[href^="#"]');
+  intLinks.forEach(link => {
+    link.addEventListener('click', scrollToID);
+  });
+
+
+  
